0d4abf6dba700e108c3a015fcdf7a2f36a04facc
troanart <troanart@gmail.com>
2023-08-11 16:21:19 +0300

    add State

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,22 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./todo-list-item.css";
+import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+
   state = {
     done: false,
-    important: false,
+    important: false
   };
 
   onLabelClick = () => {
-    this.setState({
-      done: true,
+    this.setState(({done}) => {
+      return {
+        done: !done
+      };
     });
   };
 
   onMarkImportant = () => {
-    this.setState({
-      important: true,
+    this.setState(({important}) => {
+      return {
+        important: !important
+      };
     });
   };
 
@@ -24,35 +29,35 @@ export default class TodoListItem extends Component {
     const { label } = this.props;
     const { done, important } = this.state;
 
-    let classNames = "todo-list-item ";
+    let classNames = 'todo-list-item';
     if (done) {
-      classNames += "done";
+      classNames += ' done';
     }
+
     if (important) {
-      classNames += "important";
+      classNames += ' important';
     }
 
     return (
       <span className={classNames}>
-        <span className="todo-list-item-label" onClick={this.onLabelClick}>
+        <span
+          className="todo-list-item-label"
+          onClick={ this.onLabelClick }>
           {label}
         </span>
 
-        <button
-          type="button"
-          className="btn btn-outline-success btn-sm float-right"
-          onClick={this.onMarkImportant}
-        >
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
 
-        <button
-          type="button"
-          className="btn btn-outline-danger btn-sm float-right"
-        >
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
           <i className="fa fa-trash-o" />
         </button>
       </span>
     );
-  }
+  };
 }
+
